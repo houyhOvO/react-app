dd19ae235775e0c2734d0a2abff96ab774257db5
houyaohui <u7754892@anu.edu.au>
2024-07-27 21:37:15 +1000

    updated listgroup

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,12 +1,17 @@
 import { useState } from "react";
 
-function ListGroup() {
-  let items = ["New York", "San Francisco", "Sydney", "London", "Shanghai"];
-  const [selectedIndex, setSelectedIndex] = useState(-1);   
+interface Props {
+  items: string[];
+  heading: string;
+  onSelectedItem: (item: string) => void;
+}
+
+function ListGroup({ items, heading, onSelectedItem }: Props) {
+  const [selectedIndex, setSelectedIndex] = useState(-1); // Create state variable
 
   return (
     <>
-      <h1>List</h1>
+      <h1>{heading}</h1>
       {items.length === 0 && <p>No item found</p>}
       <ul className="list-group">
         {items.map((item, index) => (
@@ -19,6 +24,7 @@ function ListGroup() {
             key={item}
             onClick={() => {
               setSelectedIndex(index);
+              onSelectedItem(item);
             }}
           >
             {item}
