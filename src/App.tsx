9d59bbaab659fb1982eabb609aac024d09eb47d1
houyaohui <u7754892@anu.edu.au>
2024-07-28 21:42:26 +1000

    updated structure and added css

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,21 @@ import { useState } from "react";
 import Alert from "./components/Alert";
 import Button from "./components/Button";
 import ListGroup from "./components/ListGroup";
+import './App.css'
 
 function App() {
   // Add listgroup component
-  // let items = ["New York", "San Francisco", "Sydney", "London", "Shanghai"];
+  let items = ["New York", "San Francisco", "Sydney", "London", "Shanghai"];
 
-  // const handleSelectedItem = (item: string) => {
-  //   console.log(item)
-  // }
+  const handleSelectedItem = (item: string) => {
+    console.log(item)
+  }
 
-  // return (
-  //   <div>
-  //     <ListGroup items={items} heading="Cities" onSelectedItem={handleSelectedItem}/>
-  //   </div>
-  // );
+  return (
+    <div>
+      <ListGroup items={items} heading="Cities" onSelectedItem={handleSelectedItem}/>
+    </div>
+  );
 
   // Add alert component
   // return (
@@ -27,16 +28,16 @@ function App() {
   // );
 
   // Add button component
-  const [alertVisiable, setAlertVisiablity] = useState(false);
+  // const [alertVisiable, setAlertVisiablity] = useState(false);
 
-  return (
-    <div>
-      {alertVisiable && (
-        <Alert onClose={() => setAlertVisiablity(false)}>My Alert</Alert>
-      )}
-      <Button onClick={() => setAlertVisiablity(true)}>My Button</Button>
-    </div>
-  );
+  // return (
+  //   <div>
+  //     {alertVisiable && (
+  //       <Alert onClose={() => setAlertVisiablity(false)}>My Alert</Alert>
+  //     )}
+  //     <Button onClick={() => setAlertVisiablity(true)}>My Button</Button>
+  //   </div>
+  // );
 }
 
 export default App;
