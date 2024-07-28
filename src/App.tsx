018a1c029b2c1cfa0d66b043e34125b3bdcc25d1
houyaohui <u7754892@anu.edu.au>
2024-07-28 13:58:46 +1000

    updated alert

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
+import Alert from "./components/Alert";
 import ListGroup from "./components/ListGroup";
 
 function App() {
+  // let items = ["New York", "San Francisco", "Sydney", "London", "Shanghai"];
+
+  // const handleSelectedItem = (item: string) => {
+  //   console.log(item)
+  // }
+
+  // return (
+  //   <div>
+  //     <ListGroup items={items} heading="Cities" onSelectedItem={handleSelectedItem}/>
+  //   </div>
+  // );
   return (
     <div>
-      <ListGroup />
+      <Alert>
+        Hello <span>World</span>
+      </Alert>
     </div>
   );
 }
