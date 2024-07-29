d81affcbb2ce590fc234be68ba8e55538241f9e6
houyaohui <u7754892@anu.edu.au>
2024-07-29 21:50:07 +1000

    added button

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import Alert from "./components/Alert";
-import Button from "./components/Button";
+import Button from "./components/Button/Button";
 import ListGroup from "./components/ListGroup";
 import "./App.css";
 import { BsFillCalendarFill } from "react-icons/bs";
@@ -41,9 +41,15 @@ function App() {
   // );
 
   // Add a calendar icon
-  return <div>
-    <BsFillCalendarFill color="cyan" size="40"/>
-  </div>;
+  // return <div>
+  //   <BsFillCalendarFill color="cyan" size="40"/>
+  // </div>;
+
+  return (
+    <div>
+      <Button onClick={() => {}}>My Button</Button>
+    </div>
+  )
 }
 
 export default App;
