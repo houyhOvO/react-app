680ae30664198ee03abc3dfa1dc66a0a90a909a2
houyaohui <u7754892@anu.edu.au>
2024-07-29 21:23:12 +1000

    added a calendar icon

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,22 @@ import { useState } from "react";
 import Alert from "./components/Alert";
 import Button from "./components/Button";
 import ListGroup from "./components/ListGroup";
-import './App.css'
+import "./App.css";
+import { BsFillCalendarFill } from "react-icons/bs";
 
 function App() {
   // Add listgroup component
-  let items = ["New York", "San Francisco", "Sydney", "London", "Shanghai"];
+  // let items = ["New York", "San Francisco", "Sydney", "London", "Shanghai"];
 
-  const handleSelectedItem = (item: string) => {
-    console.log(item)
-  }
+  // const handleSelectedItem = (item: string) => {
+  //   console.log(item)
+  // }
 
-  return (
-    <div>
-      <ListGroup items={items} heading="Cities" onSelectedItem={handleSelectedItem}/>
-    </div>
-  );
+  // return (
+  //   <div>
+  //     <ListGroup items={items} heading="Cities" onSelectedItem={handleSelectedItem}/>
+  //   </div>
+  // );
 
   // Add alert component
   // return (
@@ -38,6 +39,11 @@ function App() {
   //     <Button onClick={() => setAlertVisiablity(true)}>My Button</Button>
   //   </div>
   // );
+
+  // Add a calendar icon
+  return <div>
+    <BsFillCalendarFill color="cyan" size="40"/>
+  </div>;
 }
 
 export default App;
