4620d0863fb4bd0971a5ef8f878dd72d6e5085f4
houyhOvO <houyh081777@gmail.com>
2025-10-13 21:29:28 +0800

    feat: pass props to subcomponents by children

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,20 @@
-import ListGroup from "./components/ListGroup";
+import Alert from "./components/Alert";
+// import ListGroup from "./components/ListGroup";
 
 function App() {
-  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+  // let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
 
-  const handleSelectItem = (item: string) => {
-    console.log(item);
-  };
+  // const handleSelectItem = (item: string) => {
+  //   console.log(item);
+  // };
   return (
     <div>
-      <ListGroup
+      {/* <ListGroup
         items={items}
         heading="Cities"
         onSelectItem={handleSelectItem}
-      />
+      /> */}
+      <Alert>Hello, <strong>World!</strong></Alert>
     </div>
   );
 }
